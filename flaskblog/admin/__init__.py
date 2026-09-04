167c4a770d13bef1721108a16fd3585834b81514
prapti1101 <prapti.1629@gmail.com>
2026-09-05 04:14:18 +0600

    Update __init__.py

--- flaskblog/admin/__init__.py
+++ flaskblog/admin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
-
+# URL prefix or template can be configured if separated
 admin = Blueprint('admin', __name__)
-
+#blueprint package initialization.
 from flaskblog.admin import routes
